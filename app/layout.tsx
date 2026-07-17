1ac68c3fb487d36980d25823d9658c2271d16575
Sint <htoohtoo.slh@gmail.com>
2026-07-17 11:38:03 +0100

    feat: add IntelliJ project configuration and integrate Walone font family

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,29 @@
-import { Geist, Geist_Mono, Inter } from "next/font/google"
+import localFont from "next/font/local"
 
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { cn } from "@/lib/utils";
 
-const inter = Inter({subsets:['latin'],variable:'--font-sans'})
-
-const fontMono = Geist_Mono({
-  subsets: ["latin"],
-  variable: "--font-mono",
+const walone = localFont({
+  src: [
+    {
+      path: "../public/fonts/walone/Z06-Walone Thin.ttf",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/walone/Z06-Walone Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/walone/Z06-Walone Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+  display: "swap",
+  variable: "--font-walone",
 })
 
 export default function RootLayout({
@@ -20,7 +35,7 @@ export default function RootLayout({
     <html
       lang="en"
       suppressHydrationWarning
-      className={cn("antialiased", fontMono.variable, "font-sans", inter.variable)}
+      className={cn("antialiased", walone.variable, "font-sans")}
     >
       <body>
         <ThemeProvider>{children}</ThemeProvider>
